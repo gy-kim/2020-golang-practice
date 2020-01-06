a52bdabc36d98a2f97470491624dc57a9d37f203
Geunyoung Kim <kgysmart@gmail.com>
2020-01-06 10:03:03 +0100

    Add gobyexample

---
+++ 01/01-10/06/gobyexample.com/01_sorting_by_functions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// In order to sort by custom function in Go, we need a
+// corresponding type. Here we've created a byLength type
+// that is just an alias for the builtin []string type.
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
