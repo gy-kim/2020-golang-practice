8ac28411b8a14be20fc670214be184dc9afb8f26
Geunyoung Kim <kgysmart@gmail.com>
2020-01-08 09:33:45 +0100

    Add gobyexample.com

---
+++ 01/01-10/08/gobyexample.com/03_testing/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+// IntMin : We'll be testing this sample  implementation of an integer minimum.
+// Typically, the code we're testing would be in a source file named something like
+// untutils.go, and the test file for it would then be named intutils_test.go
+func IntMin(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func TestIntMinBasic(t *testing.T) {
+	ans := IntMin(2, -2)
+	if ans != -2 {
+		t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+	}
+}
+
+func TestIntMinTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{0, 1, 0},
+		{1, 0, 0},
+		{2, -2, -2},
+		{0, -1, -1},
+		{-1, 0, -1},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := IntMin(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
