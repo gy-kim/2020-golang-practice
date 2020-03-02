100ac7b9ec85c5d767b08f729c0ad1f2cc04d69f
Geunyoung Kim <kgysmart@gmail.com>
2020-03-02 17:00:52 +0100

    Add gobyexample.com

--- 03/01-10/01/gobyexample.com/02_switch/main.go
+++ 03/01-10/01/gobyexample.com/02_switch/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	i := 2
@@ -13,4 +16,34 @@ func main() {
 	case 3:
 		fmt.Println("three")
 	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
