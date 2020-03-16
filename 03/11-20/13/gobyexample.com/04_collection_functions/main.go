8b10ecb89714b898216184faa34e9419a08979d8
Geunyoung Kim <kgysmart@gmail.com>
2020-03-16 10:25:42 +0100

    Add gobyexample

---
+++ 03/11-20/13/gobyexample.com/04_collection_functions/main.go
@@ -0,0 +1,38 @@
+package main
+
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
+
+func Include(vs []string, t string) bool {
+	return Index(vs, t) >= 0
+}
+
+func Any(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if f(v) {
+			return true
+		}
+	}
+	return false
+}
+
+func All(vs []string, f func(string) bool) bool {
+	for _, v := range vs {
+		if !f(v) {
+			return false
+		}
+	}
+	return true
+}
+
+func Filter(vs []string, f func(string) bool) []string {
+	vsf := make([]string, 0)
+}
+
+func main() {}
