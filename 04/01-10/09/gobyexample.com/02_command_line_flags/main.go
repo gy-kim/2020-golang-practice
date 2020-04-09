55529edb49b163cfa31ae7bc06705e9fa2903385
Geunyoung Kim <kgysmart@gmail.com>
2020-04-09 10:27:39 +0200

    Add gobyexample.com

---
+++ 04/01-10/09/gobyexample.com/02_command_line_flags/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+) 
+
+// go run main.go -word=opt -numb=7 -fork -svar=flag
+func main() {
+	wordPtr := flag.String("word", "foo", "a string")
+	numPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numPtr)
+	fmt.Println("fork:", *boolPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
+}
